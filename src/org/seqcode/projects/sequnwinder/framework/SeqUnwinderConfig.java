f183591280c8468c032e6a8ec72f68ba500084a2
Shaun Mahony <mahony@psu.edu>
2021-06-15 21:33:01 -0400

    Fixing minor bug in version caller

--- src/org/seqcode/projects/sequnwinder/framework/SeqUnwinderConfig.java
+++ src/org/seqcode/projects/sequnwinder/framework/SeqUnwinderConfig.java
@@ -226,8 +226,9 @@ public class SeqUnwinderConfig implements Serializable{
 		if(ap.hasKey("h") || ap.hasKey("help") || args.length == 0){
 			System.err.println(SeqUnwinderConfig.getSeqUnwinderArgsList());
 			System.exit(1);
-		}else if(ap.hasKey("v") || ap.hasKey("version")){
-			System.out.println("SeqUnwinder version "+SeqUnwinderConfig.version+"\n\n");
+		}
+		if(ap.hasKey("v") || ap.hasKey("version")){
+			System.out.println("SeqUnwinder version "+SeqUnwinderConfig.version);
 			System.exit(1);
 		}
 		System.err.println("SeqUnwinder version "+SeqUnwinderConfig.version+"\n\n");
